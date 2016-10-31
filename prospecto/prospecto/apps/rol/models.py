95d14fd1bcdae95bcc50de78f06e5d6138effcfd
poraplz <unacuentamas16@gmail.com>
2016-10-31 10:46:36 -0300

    eliminar propectos/prospecto/apps

--- prospecto/prospecto/apps/rol/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from apps.usuarios.models import Usuarios
-
-class Rol (models.Model):
-    usuario = models.ForeignKey(Usuarios)
-    idRol = models.IntegerField()
-    descripcionRol = models.TextField()
-    tipoRol = models.CharField(max_length=15)
--- prospecto/prospecto/apps/target/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Target (models.Model):
-    idTarger = models.IntegerField ()
-    nombreTarget = models.CharField(max_length=80)
-    telefonoTarget = models.IntegerField()
-    origenTarget = models.CharField(max_length=80)
-    estadoTarget = models.BooleanField()
