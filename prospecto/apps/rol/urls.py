4c23da3cef8a7918c0fd4dc7baeffe2c81763a17
Ruben072 <orubn@outlook.es>
2016-11-14 13:54:07 -0300

    Se agrega Rol sin terminar

--- prospecto/apps/rol/urls.py
+++ prospecto/apps/rol/urls.py
@@ -1,1 +1,22 @@
-urlpatterns = []
+from django.conf.urls import url
+from apps.rol import views
+
+urlpatterns = [
+
+
+		url(r'^$', views.menu_rol, name='menu_rol'),
+
+
+		url(r'^crear_rol/$', views.crear_rol, name='crear_rol'),
+
+
+		url(r'^modificar_rol/(?P<rol_id>\d+)/$', views.modificar_rol, name='modificar_rol'),
+
+
+		url(r'^eliminar_rol/(?P<rol_id>\d+)/$', views.eliminar_rol, name='eliminar_rol'),
+
+		url(r'^listar_roles/$', views.listar_roles, name='listar_roles'),
+
+
+
+]
--- prospecto/apps/rol/views.py
+++ prospecto/apps/rol/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import Group
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required, permission_required
+
+from apps.rol.forms import RolForm
+
+@login_required
+def menu_rol(request):
+	return render(request, 'rol/menu_rol.html')
+
+@login_required
+
+def crear_rol(request):
+	if request.method == 'GET':
+		form = RolForm()
+
+	else:
+		form = RolForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('rol:listar_roles'))
+
+	return render(request, 'rol/crear_rol.html', {'form':form})
+
+@login_required
+def modificar_rol(request, rol_id):
+	rol = Group.objects.get(pk=rol_id)
+	form = RolForm(instance=rol)
+
+	if request.method=='POST':
+		form = RolForm(request.POST, instance=rol)
+
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('rol:listar_roles'))
+
+	context = {'form':form, 'rol_id':rol_id}
+	return render(request, 'rol/modificar_rol.html', context)
+
+@login_required
+
+def eliminar_rol(request, rol_id):
+	rol = Group.objects.get(pk=rol_id)
+	rol.delete()
+	return HttpResponseRedirect(reverse('rol:listar_roles'))
+
+
+@login_required
+def listar_roles(request):
+	lista_roles = Group.objects.all().order_by('name')
+	context = {'lista_roles':lista_roles}
+	return render(request, 'rol/listar_roles.html', context)
+
+
+
+
