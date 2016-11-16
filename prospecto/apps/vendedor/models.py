b61895b9495569af151f3e3f1d1d0030ed14b4a4
Ruben072 <orubn@outlook.es>
2016-11-16 15:30:17 -0300

    Se agrega buscar en articulos

--- prospecto/apps/vendedor/models.py
+++ prospecto/apps/vendedor/models.py
@@ -9,7 +9,7 @@ class Vendedor (models.Model):
     vendedor_supervisor = models.ForeignKey(Supervisor, null=True)
     nombreVendedor = models.CharField(max_length=80)
     telefonoVendedor = models.IntegerField()
-    targetToCall=models.ForeignKey(Target, null=True)
+
 
 
     def __str__(self):
