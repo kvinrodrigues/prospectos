4311653567b83d9b4ae928d153d962a3588efc31
poraplz <unacuentamas16@gmail.com>
2016-11-16 14:32:13 -0300

    agrego busqueda de palabras en listar_vendedores

--- prospecto/apps/vendedor/models.py
+++ prospecto/apps/vendedor/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+from apps.target.models import Target
 from apps.supervisor.models import Supervisor
 
 
@@ -9,5 +9,8 @@ class Vendedor (models.Model):
     vendedor_supervisor = models.ForeignKey(Supervisor, null=True)
     nombreVendedor = models.CharField(max_length=80)
     telefonoVendedor = models.IntegerField()
+    targetToCall=models.ForeignKey(Target, null=True)
+
+
     def __str__(self):
         return self.nombreVendedor
