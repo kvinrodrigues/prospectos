d3a13c7b353634c49496462b92bca2e981a146a1
poraplz <unacuentamas16@gmail.com>
2016-11-22 11:43:31 -0300

    varias cambiass

--- prospecto/apps/target/models.py
+++ prospecto/apps/target/models.py
@@ -1,11 +1,16 @@
 from django.db import models
-
+from apps.vendedor.models import Vendedor
 # Create your models here.
 class Target (models.Model):
     nombreTarget = models.CharField(max_length=80)
+    DNITarget=models.IntegerField(null=True)
+    RUCTarget=models.CharField(max_length=45,null=True)
+    DireccionTarget=models.CharField(max_length=150,null=True)
+    EmailTarget=models.EmailField(null=True)
     telefonoTarget = models.IntegerField()
     origenTarget = models.CharField(max_length=80)
     estadoTarget = models.NullBooleanField()
+    vende=models.ForeignKey(Vendedor,null=True)
     def __str__(self):
         return self.nombreTarget
 
--- prospecto/apps/vendedor/models.py
+++ prospecto/apps/vendedor/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-from apps.target.models import Target
 from apps.supervisor.models import Supervisor
 
 
