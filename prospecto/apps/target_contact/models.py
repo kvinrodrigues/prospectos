b8427fe5a39262b2a28787fe832ff326402ed139
Ruben072 <orubn@outlook.es>
2016-11-24 15:16:18 -0300

    Gestion

--- prospecto/apps/target_contact/models.py
+++ prospecto/apps/target_contact/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 from apps.articulo.models import Articulo
-from apps.vendedor.models import Vendedor
 from apps.target.models import Target
-from apps.campanha.models import Campanha
 # En este modelo se guarda cada llamada: Al que se ofrece, el vendedor que realiza la operación, y el Articulo/Servicio ofrecido
 class Target_Contact (models.Model):
-    Objetivo = models.ForeignKey(Target,null=True)
-    Vendedor = models.ForeignKey(Vendedor,null=True)
-
+    Objetivo = models.ForeignKey(Target, null=True)
     Articulo=models.ManyToManyField(Articulo)
 
 
